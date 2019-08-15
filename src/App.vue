3b66533dd4e1de8a9bfc30e85b289827c255bf05
zyrif <rubaiyat9@gmail.com>
2019-08-15 18:41:21 +0600

    Created File structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-content>
+      <h1> Deferred </h1>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    //
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
