8d6e02ca7c9304411e415055c6aec0c791ea3ed8
zyrif <rubaiyat9@gmail.com>
2019-08-20 23:39:53 +0600

    Navbar.vue formatted according to vuetify lint config

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,51 +1,74 @@
 <template>
-    <nav>
-        <v-app-bar app flat>
-            <v-app-bar-nav-icon v-on:click="toggleDrawer" class="grey--text"></v-app-bar-nav-icon>
-            <v-toolbar-title class="text-uppercase grey--text">
-                <span class="font-weight-light">P</span>
-                <span>Recog</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn text color="grey">
-                <span>Sign Out</span>
-                <v-icon right>mdi-exit-to-app</v-icon>
-            </v-btn>
-        </v-app-bar>
+  <nav>
+    <v-app-bar
+      app
+      flat
+    >
+      <v-app-bar-nav-icon
+        class="grey--text"
+        v-on:click="toggleDrawer"
+      />
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">P</span>
+        <span>Recog</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn
+        text
+        color="grey"
+      >
+        <span>Sign Out</span>
+        <v-icon right>
+          mdi-exit-to-app
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
 
-        <v-navigation-drawer app dark v-model="drawer" class="blue">
-            <v-list nav>
-                <v-list-item v-ripple v-for="item in drawerItems" v-bind:key="item.title" router v-bind:to="item.route" active-class="white--text">
-                    <v-list-item-icon>
-                        <v-icon dark> {{ item.icon }}</v-icon>
-                    </v-list-item-icon>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      dark
+      class="blue"
+    >
+      <v-list nav>
+        <v-list-item
+          v-for="item in drawerItems"
+          v-bind:key="item.title"
+          v-ripple
+          router
+          v-bind:to="item.route"
+          active-class="white--text"
+        >
+          <v-list-item-icon>
+            <v-icon dark> {{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-                    <v-list-item-content>
-                        <v-list-item-title>{{ item.title }}</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-            </v-list>
-        </v-navigation-drawer>
-    </nav>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            drawer: false,
-            drawerItems: [
-                {icon: 'mdi-view-dashboard-outline', title: 'Dashboard', route: '/'},
-                {icon: 'mdi-content-save-outline', title: 'Deferred', route: '/deferred'},
-                {icon: 'mdi-monitor', title: 'Realtime', route: '/realtime'},
-                {icon: 'mdi-settings-outline', title: 'Settings', route: '/settings'},
-            ]
-        }
+  export default {
+    data () {
+      return {
+        drawer: false,
+        drawerItems: [
+          { icon: 'mdi-view-dashboard-outline', title: 'Dashboard', route: '/' },
+          { icon: 'mdi-content-save-outline', title: 'Deferred', route: '/deferred' },
+          { icon: 'mdi-monitor', title: 'Realtime', route: '/realtime' },
+          { icon: 'mdi-settings-outline', title: 'Settings', route: '/settings' }
+        ]
+      }
     },
     methods: {
-        toggleDrawer() {
-            this.drawer = !this.drawer
-        }
+      toggleDrawer () {
+        this.drawer = !this.drawer
+      }
     }
-}
+  }
 </script>
