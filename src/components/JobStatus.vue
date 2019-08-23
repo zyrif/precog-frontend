ac5bcce6d5cccd6924a14c96639479e083621141
zyrif <rubaiyat9@gmail.com>
2019-08-23 23:52:03 +0600

    Added Job Status component

---
+++ src/components/JobStatus.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container
+    class="fill-height"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+      >
+        <v-card
+          class="elevation-2 px-12 py-8"
+        >
+          <v-layout
+            row
+            justify-center
+            align-center
+          >
+            <v-progress-circular
+              v-bind:size="100"
+              v-bind:width="7"
+              indeterminate
+              color="blue"
+            />
+          </v-layout>
+          <p class="text-center display-1 mt-7 grey--text">
+            Processing
+          </p>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    //
+  }
+</script>
+
+<style scoped>
+  .v-progress-circular {
+    margin: 1rem;
+  }
+</style>
